9f8908bb34b5abdd819a07b0cd9afdbcd65ba875
Thomas Schmidt <t.ludovic.schmidt@gmail.com>
2017-05-23 14:48:16 +1000

    Add function documentation

--- strcalc.R
+++ strcalc.R
@@ -1,4 +1,6 @@
 compute = function(input_string) {
+  # Perform simple arithmetic encoded in input string
+  # e.g. '1 + 2' -> 3, or '1 - 2' -> -1
   values = unlist(strsplit(input_string, ' '))
   num0 = as.integer(values[1])
   operator = values[2]
