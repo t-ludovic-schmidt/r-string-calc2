a5e7676d0ae96d6ff3b9ee76124abf11388ba366
Thomas Schmidt <t.ludovic.schmidt@gmail.com>
2017-05-23 15:03:42 +1000

    Add support for division

--- strcalc.R
+++ strcalc.R
@@ -8,6 +8,7 @@ compute = function(input_string) {
   result = switch(operator,
                   '+' = num0 + num1,
                   '-' = num0 - num1,
+                  '/' = num0 / num1,
                   NA)
   if (is.na(result)) {
     print('unknown operator!')
